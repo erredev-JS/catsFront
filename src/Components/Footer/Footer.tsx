b5a0b6293440e1abe88a66190bba7f1394159f8f
erredev-JS <rodrigozapata968@gmail.com>
2025-07-16 21:47:16 -0300

    Fixed styles

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="h-[8vh] bg-slate-700 flex items-center px-2 justify-center border absolute bottom-0 right-0 left-0">
+    <div className="h-[6vh] bg-slate-700 flex items-center px-2 justify-center border absolute bottom-0 right-0 left-0">
       <p className="text-xl text-white">
         Todos los derechos reservados <a href="https://www.github.com/erredev-JS" className="text-blue-500 underline cursor-pointer">erredev</a>
       </p>
